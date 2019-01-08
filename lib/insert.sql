bf08cadfb02f4cdc08aba0f7b48310975dd66a84
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 16:28:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,9 +19,9 @@ VALUES ("Eddard Stark", "Winter is coming", "human", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id)
 VALUES ("Character One", "motto one", "cylon", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id)
-VALUES ();
+VALUES ("Character Two", "motto two", "human", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id)
-VALUES ();
+VALUES ("Character Three", "motto three", "cylon", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id)
 VALUES ();
 
@@ -29,7 +29,7 @@ VALUES ();
 
 --INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
---(5, ), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
+--(5, ), (6, ), (7, ), (8, "Character Four", "motto four", "cylon", 2, 2);
 
 --INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
