852bf1e3845949b799c2b9de15983472517ca173
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 16:51:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,8 +22,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT 
-  FROM"
+  "SELECT authors.name, sungenres.name
+  FROM authors
+  JOIN series
+  ON authors.id = series.author_id"
 end
 
 def select_series_title_with_most_human_characters
