2721aedcef9870f59f0c83a2c1db7f82b96e05ab
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 16:16:25 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -11,3 +11,9 @@ CREATE TABLE subgenres
 CREATE TABLE authors
 (id INTEGER PRIMARY KEY,
   name TEXT);
+
+CREATE TABLE books
+(id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER
+  series_id INTEGER);
