7d3ee5eff2db798e9107e2f5e35c684bf4c430a0
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 16:21:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,7 @@
 INSERT INTO series (title, author_id, subgenre_id)
   VALUES ("A Song of Ice and Fire", 1, 1);
 INSERT INTO series (title, author_id, subgenre_id)
-  VALUES ();
-
-
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
+  VALUES ("Second Series", 2, 2);
 
 INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 
