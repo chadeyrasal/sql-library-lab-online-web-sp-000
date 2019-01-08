c234d226323e464dc7646abaeef9793d1a438f3d
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-08 16:51:59 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,9 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, sungenres.name
   FROM authors
   JOIN series
-  ON authors.id = series.author_id"
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON series.subgenre_id = subgenres.name"
 end
 
 def select_series_title_with_most_human_characters
